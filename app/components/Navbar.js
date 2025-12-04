683d4c22721221b745c3366b7f6d8f35cbbb9919
qasimleoo <qasimmirza8968807@gmail.com>
2025-12-04 12:00:14 +0500

    first commit

---
+++ app/components/Navbar.js
@@ -0,0 +1,16 @@
+
+
+export function Test () {
+    return <div>
+        <h1>this is a new h1</h1>
+
+    </div>
+}
+
+export const Test2 = () => {
+    return <div>
+        <h1>
+            This is Test2
+        </h1>
+    </div>
+}
---
+++ app/testPage/page.js
@@ -0,0 +1,8 @@
+import { Test2, Test } from "../components/Navbar"
+
+export default function Name () {
+    
+    return <div>
+        <h1>This is Test Page</h1>
+    </div>
+}
